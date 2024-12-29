352e69ba51727f4b562e7df7faae0754c9b54660
vaibhav-25 <vaibhavsinghvi254@gmail.com>
2024-12-29 20:07:57 +0530

    fix: title fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<>
 			<Head>
-				<title>Collabio | Online Whiteboard</title>
+				<title>QuickCollab | Online Whiteboard</title>
 				<link
 					rel='icon'
 					href='/favicon.ico'

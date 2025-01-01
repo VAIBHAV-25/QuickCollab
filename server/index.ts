88a73278947f42c9f81659fa5cfff51035964b55
vaibhav-25 <vaibhavsinghvi254@gmail.com>
2025-01-01 10:51:52 +0530

    fix: minor fixes

--- server/index.ts
+++ server/index.ts
@@ -8,8 +8,7 @@ import { Server } from 'socket.io';
 import { v4 } from 'uuid';
 
 const port = parseInt(process.env.PORT || '3000', 10);
-// const dev = process.env.NODE_ENV !== 'production';
-const dev = true;
+const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler: NextApiHandler = nextApp.getRequestHandler();
 
